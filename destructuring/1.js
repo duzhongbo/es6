7b029d66ebef05893cfe5dc18c51b3f3c518aa58
duzhongbo <710409599@qq.com>
2015-12-18 14:36:25 +0800

    解构3

--- destructuring/1.js
+++ destructuring/1.js
@@ -7,12 +7,15 @@
 
 // 解构2
 
-let [foo, [[bar], baz]] = [1, [[2], 3]];
-console.log(foo);// 1
-console.log(bar);// 2
-console.log(baz);// 3
+// let [foo, [[bar], baz]] = [1, [[2], 3]];
+// console.log(foo);// 1
+// console.log(bar);// 2
+// console.log(baz);// 3
 
 
+// 解构3
+let [ , , third] = ["foo", "bar", "baz"];
+console.log(third);
 
 
 
