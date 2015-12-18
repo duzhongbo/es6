36144f678a420df10d240049543e4a4405ebca6c
duzhongbo <710409599@qq.com>
2015-12-18 12:32:18 +0800

    const声明的变量不得改变值，这意味着，const一旦声明变量，就必须立即初始化，不能留到以后赋值

--- const.js
+++ const.js
@@ -1,7 +1,12 @@
 'use strict';
 
-const PI = 3.1415;
-// console.log(PI);// 3.1415
+// 例子1
+// const PI = 3.1415;
+// // console.log(PI);// 3.1415
 
 
-PI = 3;// 报错
+// PI = 3;// 报错
+
+// 例子2
+// const foo;// 报错
+const foo=1;
