2602acdc00f82809be3ea63b62cd8caa0136b7f1
duzhongbo <710409599@qq.com>
2015-12-18 11:36:58 +0800

    let命令只在命令所在的代码块内有效2

--- let.js
+++ let.js
@@ -1,7 +1,17 @@
 'use strict';
-{
-  let a = 10;
-  var b = 1;
-}
-console.log(b);// 1
-console.log(a);// ReferenceError: a is not defined.
+// 例子1
+// {
+//   let a = 10;
+//   var b = 1;
+// }
+// console.log(b);// 1
+// console.log(a);// ReferenceError: a is not defined.
+
+
+// 例子2
+// for(let i = 0; i < 3; i++){
+// 	console.log(i);// 0,1,2
+// }
+// console.log(i);// ReferenceError: i is not defined.
+
+
